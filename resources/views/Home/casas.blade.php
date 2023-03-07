096fcd323666dc1f4e8da5e3797840779b6e2a0b
Hinmorlath <josepolomanuel2@gmail.com>
2023-03-06 21:48:58 -0600

    estilos de Inicio de Sesion y casas

--- resources/views/Home/casas.blade.php
+++ resources/views/Home/casas.blade.php
@@ -6,22 +6,25 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="shortcut icon" href="img/rottemX_icono.png" type="png">
     <link rel="stylesheet" href="iconos/css/all.css">
+    <link rel="stylesheet" href="css/estilosC.css">
     <title>Mis casas</title>
 </head>
-<body>
-    <header>
-        <span>Usuario</span>
-        <span class="fa-solid fa-gear"></span>
+<body class="body_C">
+    <header class="header_C">
+        <figure>
+            <a href="#"></a>
+            <img src="img/rottemX.png" alt="RottemX">
+        </figure>
+        <span class="n_usuario">Usuario</span>
+        <span class="fa-solid fa-gear ajustes"></span>
     </header>
-    <main>
-        <article>
-            <a href="#">
+    <main class="main_C">
+        <span class="titulo_C">Mis casas</span>
+        <a href="#">
+            <article class="article_Agregar">
                 <span class="fa-solid fa-plus"></span>
-            </a>
-        </article>
+            </article>
+        </a>
     </main>
-    <aside>
-        <span>Casas</span>
-    </aside>
 </body>
 </html>
--- resources/views/Home/iniciarSesion.blade.php
+++ resources/views/Home/iniciarSesion.blade.php
@@ -11,45 +11,54 @@
 </head>
 <body class="body_IS">
     <main class="main_IS">
-        <figure>
+        <figure class="logoMain">
             <img src="img/rottemX.png" alt="RottemX">
         </figure>
     </main>
     <aside class="aside_IS">
         <span class="btnIS">Inicio de Sesión</span>
         <span class="btnR">Registrase</span>
-        <form class="formIS" action="#" method="post">
-            <label for="emailUsuario_IS">
-                Usuario:
-            </label>
-            <input type="email" name="emailUsuario" id="emailUsuario_IS" placeholder="Escribe tu correo" autofocus>
-            <label for="pass_IS">
-                Contraseña:
-            </label>
-            <input type="password" name="pass_IS" id="pass_IS" placeholder="Escribe tu contraseña">
-            <input class="btnCancelar" type="reset" value="Cancelar">
-            <button class="btnGuardar" type="submit">
-                <span class="fa-solid fa-floppy-disk"></span>
-                <span>Guardar</span>
-            </button>
-        </form>
-        <form class="formR" action="#" method="post">
-            <input type="file" name="imgUsuario_IS" id="imgUsuario_IS">
-            <span class="fa-solid fa-plus"></span>
-            <label for="usuario_R">Usuario:</label>
-            <input type="text" name="usuario_R" id="usuario_R" placeholder="Nombre de usuario">
-            <label for="emailUsuario_R">Correo electrónico:</label>
-            <input type="email" name="emailUsuario_R" id="emailUsuario_R" placeholder="Correo electrónico" autocomplete>
-            <label for="pass_R">Contraseña:</label>
-            <input type="password" name="pass_R" id="pass_R" placeholder="Ingresa una contraseña" minlength="6">
-            <label for="telefono_R">Teléfono:</label>
-            <input type="text" name="telUsuario_R" id="telUsuario_R" placeholder="Ingresa un número telefónico">
-            <input class="btnCancelar" type="reset" value="Cancelar">
-            <button class="btnGuardar" type="submit">
-                <span class="fa-solid fa-floppy-disk"></span>
-                <span>Guardar</span>
-            </button>
-        </form>
+        <div class="boxR" id="boxR">
+            <!--Este es el formulario del inicio de Sesión-->
+            <form class="formIS" action="#" method="post">
+                <label for="emailUsuario_IS">
+                    Usuario:
+                </label>
+                <!--Input de Correo del usuario-->
+                <input type="email" name="emailUsuario" id="emailUsuario_IS" placeholder="Escribe tu correo" autofocus required>
+                <label for="pass_IS">
+                    Contraseña:
+                </label>
+                <!--Input de Correo de la contraseña-->
+                <input type="password" name="pass_IS" id="pass_IS" placeholder="Escribe tu contraseña" required>
+                <input class="btnCancelar" type="reset" value="Cancelar">
+                <input class="btnGuardar" type="submit" value="Iniciar">
+            </form>
+
+            <!--Este es el formulario de Registro-->
+            <form class="formR" action="#" method="post">
+                <!--Input de la imagen-->
+                <input type="file" name="imgUsuario_IS" id="imgUsuario_IS">
+                <span class="fa-solid fa-plus"></span>
+                <label for="usuario_R">Usuario:</label>
+                <!--Input del Usuario-->
+                <input type="text" name="usuario_R" id="usuario_R" placeholder="Nombre de usuario">
+                <label for="emailUsuario_R">Correo electrónico:</label>
+                <!--Input de Correo del usuario-->
+                <input type="email" name="emailUsuario_R" id="emailUsuario_R" placeholder="Correo electrónico" autocomplete required>
+                <label for="pass_R">Contraseña:</label>
+                <!--Input de Correo de la contraseña-->
+                <input type="password" name="pass_R" id="pass_R" placeholder="Ingresa una contraseña" minlength="6" required>
+                <label for="confirmaPass_R">Confirmación:</label>
+                <!--Input de Confirmación de la contraseña-->
+                <input type="pass" name="confirmaPass_R" id="confirmaPass_R" placeholder="Confirma tu contraseña">
+                <input class="btnCancelar" type="reset" value="Cancelar">
+                <button class="btnGuardar" type="submit">
+                    <span class="fa-solid fa-floppy-disk"></span>
+                    <span>Guardar</span>
+                </button>
+            </form>
+        </div>
     </aside>
 </body>
 </html>
